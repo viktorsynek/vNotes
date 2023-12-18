2d9b9eaeacfed0d316be9ac60038f115dded1f5d
viktorsynek <ktor.syn@gmail.com>
2023-12-18 14:33:10 +0100

    add /home and /create links

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import PageNotFound from './components/404';
 import Register from './components/Register';
 import Login from './components/Login';
+import Home from './components/Home';
+import Create from './components/Create';
 import {Route, Routes, useNavigate} from 'react-router-dom';
 
 function RedirectToRegister() {
@@ -22,6 +24,8 @@ function App() {
         <Route path="/" element={<RedirectToRegister/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
+        <Route path="/home" element={<Home/>}></Route>
+        <Route path="/create" element={<Create/>}></Route>
 
         <Route path="*" element={<PageNotFound />} />
       </Routes>
